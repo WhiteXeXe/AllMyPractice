95b0eed9638d289c76140c617f29c360b1332ebf
Дмитрий <dimakuzmin2885@gmail.com>
2023-11-05 01:41:10 +0300

    Change

---
+++ C/IBKSchess/cmake-build-debug/CMakeFiles/IBKSchess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IBKSchess.dir/main.c.obj"
+  "IBKSchess.exe"
+  "IBKSchess.exe.manifest"
+  "IBKSchess.lib"
+  "IBKSchess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IBKSchess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
